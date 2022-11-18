05440156ea74f93f62b7abcab15e5d98b04c1eb5
Gabriel Santos <116666308+gabrells@users.noreply.github.com>
2022-11-17 22:20:46 -0300

    Graphic layout creation
    Graphic layout creation with JavaScript and Figma

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-function enserirEmail() {
-    document.querySelector('.email');
+function imgGrafic(grafico1, grafico2) {
+    return `
+        <li class="grafico">
+            <img src="./assets/grafico-${grafico1}.svg">
+            <img src="./assets/grafico-${grafico2}.svg">
+        </li>
+    `
 }
+
+let delay = -0.4;
+function createGrafic(painel) {
+    delay = delay + 0.4;
+     return `
+         <div class="painel" style="animation-delay: ${delay}s;";>
+             <ul>
+                 ${painel}
+
+            </ul>
+         </div>
+     `
+   }
+
+    document.querySelector('#layout').innerHTML = createGrafic(imgGrafic("1", "2") + imgGrafic("3", "4"))
