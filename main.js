0c426241d59856c3b559154903098ee71b2bf338
Gabriel Santos <116666308+gabrells@users.noreply.github.com>
2022-11-21 21:35:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -34,26 +34,26 @@ form.addEventListener('submit', (event) => {
 }) 
 
 
-// function imgGrafic(grafico1, grafico2) {
-//     return `
-//         <li class="grafico">
-//             <img src="./assets/grafico-${grafico1}.svg">
-//             <img src="./assets/grafico-${grafico2}.svg">
-//         </li>
-//     `
-// }
+/* function imgGrafic(grafico1, grafico2) {
+     return `
+         <li class="grafico">
+             <img src="./assets/grafico-${grafico1}.svg">
+             <img src="./assets/grafico-${grafico2}.svg">
+         </li>
+     `
+ }
 
-// let delay = -0.4;
-// function createGrafic(painel) {
-//     delay = delay + 0.4;
-//      return `
-//          <div class="painel" style="animation-delay: ${delay}s;";>
-//              <ul>
-//                  ${painel}
+ let delay = -0.4;
+ function createGrafic(painel) {
+     delay = delay + 0.4;
+      return `
+          <div class="painel" style="animation-delay: ${delay}s;";>
+              <ul>
+                  ${painel}
 
-//             </ul>
-//          </div>
-//      `
-//    }
+             </ul>
+          </div>
+      `
+    }
 
-//     document.querySelector('#layout').innerHTML = createGrafic(imgGrafic("1", "2") + imgGrafic("3", "4"))
+     document.querySelector('#layout').innerHTML = createGrafic(imgGrafic("1", "2") + imgGrafic("3", "4"))*/
